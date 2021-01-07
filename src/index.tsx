6832653d79a164d3e507685482d2364a30a5f497
math2018avan <math2018avan@gmail.com>
2021-01-07 19:01:15 +0800

    Create index.tsx
    main file

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router } from "react-router-dom";
+import App from "./app/layout/App";
+import reportWebVitals from "./reportWebVitals";
+import "semantic-ui-css/semantic.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import "./app/layout/styles.css";
+import history from "./history";
+import ScrollToTop from "./app/layout/ScrollToTop";
+
+ReactDOM.render(
+  <Router history={history}>
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
+  </Router>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
