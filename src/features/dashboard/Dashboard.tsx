309010796fd62b1e763a2469510c19a0477501a9
math2018avan <math2018avan@gmail.com>
2021-01-07 19:00:04 +0800

    dummy pages added

---
+++ src/features/dashboard/Dashboard.tsx
@@ -0,0 +1,7 @@
+import { observer } from "mobx-react-lite";
+
+const Dashboard = () => {
+  return <p>THIS IS SOME TESTING</p>;
+};
+
+export default observer(Dashboard);
---
+++ src/features/home/Test.tsx
@@ -0,0 +1,5 @@
+const Test = () => {
+  return <div>This is Test</div>;
+};
+
+export default Test;
