88db3d7eb460a0fe9fc86c872bfba27ed431bea9
math2018avan <math2018avan@gmail.com>
2021-01-16 17:24:21 +0800

    Create constants.ts
    constant values added

---
+++ src/app/models/constants.ts
@@ -0,0 +1,12 @@
+
+//Access roles
+export const SUPER_ADMIN = "SuperAdmin";
+export const ADMIN = "Admin";
+export const REPORT = "Report";
+export const NORMAL = "Normal";
+
+//PO Status
+export const PURCHASE_ORDER_PENDING = "Pending";
+export const PURCHASE_ORDER_APPROVED = "Approved";
+export const PURCHASE_ORDER_CANCELLED = "Cancelled";
+export const PURCHASE_ORDER_REJECTED = "Rejected";
