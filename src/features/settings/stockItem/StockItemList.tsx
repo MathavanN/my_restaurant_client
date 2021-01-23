4ba41a6184a8a1111816d72ff0fba3f7a862142a
math2018avan <math2018avan@gmail.com>
2021-01-23 12:36:14 +0800

    Update StockItemList.tsx
    stock type check added
    initial load check added

--- src/features/settings/stockItem/StockItemList.tsx
+++ src/features/settings/stockItem/StockItemList.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext } from "react";
+import React, { Fragment, useContext, useEffect, useState } from "react";
 import { observer } from "mobx-react-lite";
 import { RootStoreContext } from "../../../app/stores/rootStore";
 import { Table } from "semantic-ui-react";
@@ -7,6 +7,7 @@ import FilterStockItem from "./FilterStockItem";
 import StockItemListHeader from "./StockItemListHeader";
 import StockItemListItem from "./StockItemListItem";
 import StockListItemFooter from "./StockListItemFooter";
+import { LoadingComponent } from "../../../app/layout/LoadingComponent";
 
 const StockItemList = () => {
   const rootStore = useContext(RootStoreContext);
@@ -17,45 +18,69 @@ const StockItemList = () => {
 
   const {
     loadStockItems,
-    loadStockItems1,
     getStockItems,
     getStockItemTotalPages,
     page,
     setStockItemPage,
-    setPredicate,
+    loadingInitial,
   } = rootStore.stockItemStore;
   const { openModal } = rootStore.modalStore;
   const { hasModifyAccess } = rootStore.userStore;
+  const [selectedStockType, setSelectedStockType] = useState(0);
+
+  const handleStockItemSearch = (typeId: number) => {
+    setStockItemPage(1);
+    setSelectedStockType(typeId);
+    loadStockItems(typeId);
+  };
+
+  const handleOnPageChange = (page: number) => {
+    setStockItemPage(page);
+    loadStockItems(selectedStockType);
+  };
+
+  useEffect(() => {
+    if (loadStockTypeOptions.length > 0) {
+      setSelectedStockType(loadStockTypeOptions[0].key);
+      loadStockItems(loadStockTypeOptions[0].key);
+    }
+  }, [loadStockTypeOptions, loadStockItems]);
+
+  if (loadStockTypeOptions.length === 0)
+    return <LoadingComponent content="Cannot find stock type..." />;
+
+  if (loadingInitial && page === 1)
+    return <LoadingComponent content="Loading stock items..." />;
 
   return (
     <Fragment>
       <FilterStockItem
         stockTypeOptions={loadStockTypeOptions}
-        setPredicate={setPredicate}
-        loadStockItems={loadStockItems1}
+        handleStockItemSearch={handleStockItemSearch}
       />
-      <Table compact celled>
-        <StockItemListHeader
-          hasModifyAccess={hasModifyAccess}
-          openModal={openModal}
-          stockItem={new StockItemFormValues()}
-          stockTypeOptions={loadStockTypeOptions}
-          unitOfMeasureOptions={loadUnitOfMeasureOptions}
-        />
-        <StockItemListItem
-          hasModifyAccess={hasModifyAccess}
-          openModal={openModal}
-          getStockItems={getStockItems}
-          stockTypeOptions={loadStockTypeOptions}
-          unitOfMeasureOptions={loadUnitOfMeasureOptions}
-        />
-        <StockListItemFooter
-          page={page}
-          totalPages={getStockItemTotalPages}
-          setPage={setStockItemPage}
-          loadStockItems={loadStockItems}
-        />
-      </Table>
+      {selectedStockType > 0 && (
+        <Table compact celled>
+          <StockItemListHeader
+            hasModifyAccess={hasModifyAccess}
+            openModal={openModal}
+            stockItem={new StockItemFormValues()}
+            stockTypeOptions={loadStockTypeOptions}
+            unitOfMeasureOptions={loadUnitOfMeasureOptions}
+          />
+          <StockItemListItem
+            hasModifyAccess={hasModifyAccess}
+            openModal={openModal}
+            getStockItems={getStockItems}
+            stockTypeOptions={loadStockTypeOptions}
+            unitOfMeasureOptions={loadUnitOfMeasureOptions}
+          />
+          <StockListItemFooter
+            page={page}
+            totalPages={getStockItemTotalPages}
+            handleOnPageChange={handleOnPageChange}
+          />
+        </Table>
+      )}
     </Fragment>
   );
 };
