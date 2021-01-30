292168903145908eda09b34cae09151e246ad74a
math2018avan <math2018avan@gmail.com>
2021-01-30 20:43:35 +0800

    Create PaymentTypeListHeader.tsx
    payment type list header added

---
+++ src/features/settings/paymentType/PaymentTypeListHeader.tsx
@@ -0,0 +1,45 @@
+import React, { FC, Fragment } from "react";
+import { Button, Icon, Table } from "semantic-ui-react";
+import { PaymentTypeFormValues } from "../../../app/models/paymentType";
+import  EditPaymentType  from "./EditPaymentType";
+
+interface IProps {
+  hasModifyAccess: boolean;
+  paymentType: PaymentTypeFormValues;
+  openModal: (content: any) => void;
+}
+const PaymentTypeListHeader: FC<IProps> = ({
+  hasModifyAccess,
+  paymentType,
+  openModal,
+}) => {
+  return (
+    <Fragment>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>No</Table.HeaderCell>
+          <Table.HeaderCell>Name</Table.HeaderCell>
+          <Table.HeaderCell>Credit Period</Table.HeaderCell>
+          {hasModifyAccess && (
+            <Table.HeaderCell>
+              <Button
+                animated="vertical"
+                color="green"
+                onClick={() =>
+                  openModal(<EditPaymentType paymentType={paymentType} />)
+                }
+              >
+                <Button.Content hidden>Add</Button.Content>
+                <Button.Content visible>
+                  <Icon name="add circle" />
+                </Button.Content>
+              </Button>
+            </Table.HeaderCell>
+          )}
+        </Table.Row>
+      </Table.Header>
+    </Fragment>
+  );
+};
+
+export default PaymentTypeListHeader;
