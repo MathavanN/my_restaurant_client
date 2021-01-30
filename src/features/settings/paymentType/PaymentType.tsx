d0c4fd0a102375a1fb7a84a9b880f37b8d511f15
math2018avan <math2018avan@gmail.com>
2021-01-30 20:42:58 +0800

    Create PaymentType.tsx
    payment type added

---
+++ src/features/settings/paymentType/PaymentType.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import PaymentTypeList from "./PaymentTypeList";
+
+const PaymentType = () => {
+  return (
+    <Fragment>
+      <PaymentTypeList />
+    </Fragment>
+  );
+};
+
+export default PaymentType;
