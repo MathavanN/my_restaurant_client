89ffb1c630a74b0a5a604e969d4c477e20909dcc
math2018avan <math2018avan@gmail.com>
2021-02-05 17:52:06 +0800

    Update GRNListItem.tsx
    grn edit added

--- src/features/grn/GRNListItem.tsx
+++ src/features/grn/GRNListItem.tsx
@@ -1,20 +1,39 @@
-import React, { FC, Fragment } from "react";
+import React, { FC, Fragment, useContext } from "react";
 import { Button, Icon, Table } from "semantic-ui-react";
-import { IGoodsReceivedNote } from "../../app/models/goodsReceivedNote";
+import {
+  CreateGoodsReceivedNote,
+  IGoodsReceivedNote,
+} from "../../app/models/goodsReceivedNote";
 import { format, isEqual } from "date-fns";
 import { Link } from "react-router-dom";
 import { PENDING } from "../../app/models/constants";
+import { RootStoreContext } from "../../app/stores/rootStore";
+import { observer } from "mobx-react-lite";
+import AddGRN from "./AddGRN";
 
 interface IProps {
   grns: [string, IGoodsReceivedNote][];
+  displayColumn: boolean;
+  displayView: boolean;
+  displayEdit: boolean;
 }
-const GRNListItem: FC<IProps> = ({ grns }) => {
+const GRNListItem: FC<IProps> = ({
+  grns,
+  displayEdit,
+  displayView,
+  displayColumn,
+}) => {
+  const rootStore = useContext(RootStoreContext);
+  const { openModal } = rootStore.modalStore;
+  const { loadPaymentTypeOptions } = rootStore.settingsStore;
+  const { loadAppUsersOptions } = rootStore.userStore;
+  const { loadApprovedPurchaseOrdersOptions } = rootStore.purchaseOrderStore;
   return (
     <Fragment>
       <Table.Body>
         {grns.map(([group, grn]) => (
           <Table.Row key={grn.id}>
-            <Table.Cell>{group}</Table.Cell>
+            {displayColumn && <Table.Cell>{group}</Table.Cell>}
             <Table.Cell>{grn.purchaseOrderNumber}</Table.Cell>
             <Table.Cell>{grn.invoiceNumber}</Table.Cell>
             <Table.Cell>{grn.paymentTypeName}</Table.Cell>
@@ -24,21 +43,48 @@ const GRNListItem: FC<IProps> = ({ grns }) => {
             <Table.Cell negative={grn.approvalStatus === PENDING}>
               {grn.approvalStatus}
             </Table.Cell>
-            <Table.Cell>{grn.approvedUserName}</Table.Cell>
-            <Table.Cell>
-              {!isEqual(
-                new Date(grn.approvedDate),
-                new Date("0001-01-01T00:00:00")
-              ) && format(new Date(grn.approvedDate), "yyyy-MM-dd'T'HH:mm")}
-            </Table.Cell>
-            <Table.Cell>
-              <Button
-                content="View"
-                color="blue"
-                as={Link}
-                to={`/grn/view/${grn.id}`}
-              />
-            </Table.Cell>
+            {displayColumn && <Table.Cell>{grn.approvedUserName}</Table.Cell>}
+            {displayColumn && (
+              <Table.Cell>
+                {!isEqual(
+                  new Date(grn.approvedDate),
+                  new Date("0001-01-01T00:00:00")
+                ) && format(new Date(grn.approvedDate), "yyyy-MM-dd'T'HH:mm")}
+              </Table.Cell>
+            )}
+            {displayView && (
+              <Table.Cell>
+                <Button
+                  content="View"
+                  color="blue"
+                  as={Link}
+                  to={`/grn/view/${grn.id}`}
+                />
+              </Table.Cell>
+            )}
+            {displayEdit && (
+              <Table.Cell>
+                <Button
+                  animated="vertical"
+                  color="orange"
+                  onClick={() =>
+                    openModal(
+                      <AddGRN
+                        goodsReceivedNote={new CreateGoodsReceivedNote(grn)}
+                        purchaseOrderOptions={loadApprovedPurchaseOrdersOptions}
+                        paymentTypeOptions={loadPaymentTypeOptions}
+                        userOptions={loadAppUsersOptions}
+                      />
+                    )
+                  }
+                >
+                  <Button.Content hidden>Edit</Button.Content>
+                  <Button.Content visible>
+                    <Icon name="edit" />
+                  </Button.Content>
+                </Button>
+              </Table.Cell>
+            )}
           </Table.Row>
         ))}
       </Table.Body>
@@ -46,4 +92,4 @@ const GRNListItem: FC<IProps> = ({ grns }) => {
   );
 };
 
-export default GRNListItem;
+export default observer(GRNListItem);
