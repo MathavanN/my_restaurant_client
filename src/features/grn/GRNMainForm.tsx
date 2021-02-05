dfdd8bf8876a3aac136c5ab976a0a2d780c1871e
math2018avan <math2018avan@gmail.com>
2021-02-05 17:52:38 +0800

    Update GRNMainForm.tsx
    mange grn added

--- src/features/grn/GRNMainForm.tsx
+++ src/features/grn/GRNMainForm.tsx
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { FC, Fragment, useContext, useEffect } from "react";
+import { RouteComponentProps } from "react-router-dom";
+import { RootStoreContext } from "../../app/stores/rootStore";
+import GRNList from "./GRNList";
 
-const GRNMainForm = () => {
+interface IDetailsParams {
+  id: string;
+}
+
+const GRNMainForm: FC<RouteComponentProps<IDetailsParams>> = ({
+  match,
+  history,
+}) => {
+  const rootStore = useContext(RootStoreContext);
+  const { loadGRN, loadGRNItems, loadGRNFreeItems, grn } = rootStore.grnStore;
+  const { loadPaymentTypes } = rootStore.settingsStore;
+  const { loadAppUsers } = rootStore.userStore;
+  const { loadPurchaseOrders } = rootStore.purchaseOrderStore;
+  useEffect(() => {
+    loadPaymentTypes();
+    loadAppUsers();
+    loadPurchaseOrders();
+    if (match.params.id) {
+      loadGRN(parseInt(match.params.id));
+      loadGRNItems(parseInt(match.params.id));
+      loadGRNFreeItems(parseInt(match.params.id));
+    }
+  }, [
+    loadPaymentTypes,
+    loadAppUsers,
+    loadPurchaseOrders,
+    loadGRN,
+    loadGRNItems,
+    loadGRNFreeItems,
+    match.params.id,
+  ]);
   return (
-    <div>
-      <p>Hi Hi</p>
-    </div>
+    <Fragment>
+      {match.params.id && grn! && (
+        <Fragment>
+          <GRNList
+            grns={new Array(["1", grn])}
+            displayColumn={false}
+            displayEdit={true}
+            displayView={false}
+          />
+        </Fragment>
+      )}
+    </Fragment>
   );
 };
 
