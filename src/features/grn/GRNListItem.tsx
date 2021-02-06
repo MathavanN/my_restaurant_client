7870a5322d99ebfa38607583470cb40f38445446
math2018avan <math2018avan@gmail.com>
2021-02-06 19:15:42 +0800

    Update GRNListItem.tsx
    superadmin check added

--- src/features/grn/GRNListItem.tsx
+++ src/features/grn/GRNListItem.tsx
@@ -26,7 +26,7 @@ const GRNListItem: FC<IProps> = ({
   const rootStore = useContext(RootStoreContext);
   const { openModal } = rootStore.modalStore;
   const { loadPaymentTypeOptions } = rootStore.settingsStore;
-  const { loadAppUsersOptions } = rootStore.userStore;
+  const { loadAppUsersOptions, isSuperAdminUser } = rootStore.userStore;
   const { loadApprovedPurchaseOrdersOptions } = rootStore.purchaseOrderStore;
   return (
     <Fragment>
@@ -62,7 +62,7 @@ const GRNListItem: FC<IProps> = ({
                 />
               </Table.Cell>
             )}
-            {displayEdit && (
+            {displayEdit && isSuperAdminUser && (
               <Table.Cell>
                 <Button
                   animated="vertical"
