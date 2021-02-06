5c1e2ffcae8267344ba9bbe6a31803310a74a792
math2018avan <math2018avan@gmail.com>
2021-02-06 19:13:32 +0800

    Update GRNItemDetails.tsx
    props added

--- src/features/grn/grnItem/GRNItemDetails.tsx
+++ src/features/grn/grnItem/GRNItemDetails.tsx
@@ -1,14 +1,33 @@
-import React, { Fragment } from "react";
+import React, { FC, Fragment } from "react";
 import { Header } from "semantic-ui-react";
+import { ISelectInputOptions } from "../../../app/models/common";
+import { IGoodsReceivedNote } from "../../../app/models/goodsReceivedNote";
 import GRNItemList from "./GRNItemList";
 
-const GRNItemDetails = () => {
+interface IProps {
+  displayAmount: boolean;
+  displayAction: boolean;
+  grn: IGoodsReceivedNote;
+  stockTypeOptions: ISelectInputOptions[];
+}
+
+const GRNItemDetails: FC<IProps> = ({
+  displayAction,
+  displayAmount,
+  grn,
+  stockTypeOptions,
+}) => {
   return (
     <Fragment>
       <Header size="medium" textAlign="center">
         GRN Item Details.
       </Header>
-      <GRNItemList />
+      <GRNItemList
+        displayAction={displayAction}
+        displayAmount={displayAmount}
+        grn={grn}
+        stockTypeOptions={stockTypeOptions}
+      />
     </Fragment>
   );
 };
