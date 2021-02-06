5a093adedc8a8442f7e5ebc3af39718c4271d025
math2018avan <math2018avan@gmail.com>
2021-02-06 19:14:19 +0800

    Update GRNItemListHeader.tsx
    create grn link added

--- src/features/grn/grnItem/GRNItemListHeader.tsx
+++ src/features/grn/grnItem/GRNItemListHeader.tsx
@@ -1,11 +1,25 @@
-import React, { FC, Fragment } from "react";
-import { Table } from "semantic-ui-react";
+import React, { FC, Fragment, useContext } from "react";
+import { Button, Icon, Table } from "semantic-ui-react";
+import { ISelectInputOptions } from "../../../app/models/common";
+import { IGoodsReceivedNote } from "../../../app/models/goodsReceivedNote";
+import { GoodsReceivedNoteItemFormValues } from "../../../app/models/goodsReceivedNoteItem";
+import { RootStoreContext } from "../../../app/stores/rootStore";
+import CreateGRNItem from "./CreateGRNItem";
 
 interface IProps {
   displayAmount: boolean;
   displayAction: boolean;
+  grn: IGoodsReceivedNote;
+  stockTypeOptions: ISelectInputOptions[];
 }
-const GRNItemListHeader: FC<IProps> = ({ displayAction, displayAmount }) => {
+const GRNItemListHeader: FC<IProps> = ({
+  displayAction,
+  displayAmount,
+  grn,
+  stockTypeOptions,
+}) => {
+  const rootStore = useContext(RootStoreContext);
+  const { openModal } = rootStore.modalStore;
   return (
     <Fragment>
       <Table.Header>
@@ -19,7 +33,27 @@ const GRNItemListHeader: FC<IProps> = ({ displayAction, displayAmount }) => {
           <Table.HeaderCell>VAT %</Table.HeaderCell>
           <Table.HeaderCell>Discount %</Table.HeaderCell>
           {displayAmount && <Table.HeaderCell>Amount</Table.HeaderCell>}
-          {displayAction && <Table.HeaderCell>Action</Table.HeaderCell>}
+          {displayAction && (
+            <Table.HeaderCell textAlign="center">
+              <Button
+                animated="vertical"
+                color="green"
+                onClick={() =>
+                  openModal(
+                    <CreateGRNItem
+                      item={new GoodsReceivedNoteItemFormValues(grn.id)}
+                      stockTypeOptions={stockTypeOptions}
+                    />
+                  )
+                }
+              >
+                <Button.Content hidden>Add</Button.Content>
+                <Button.Content visible>
+                  <Icon name="add circle" />
+                </Button.Content>
+              </Button>
+            </Table.HeaderCell>
+          )}
         </Table.Row>
       </Table.Header>
     </Fragment>
