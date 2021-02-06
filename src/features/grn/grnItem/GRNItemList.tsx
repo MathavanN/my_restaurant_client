8aacc4f5697c29b0dedee0bc7cc42df060f15e81
math2018avan <math2018avan@gmail.com>
2021-02-06 19:13:41 +0800

    Update GRNItemList.tsx
    props added

--- src/features/grn/grnItem/GRNItemList.tsx
+++ src/features/grn/grnItem/GRNItemList.tsx
@@ -1,14 +1,38 @@
-import React, { Fragment } from "react";
+import React, { Fragment, FC } from "react";
 import { Table } from "semantic-ui-react";
+import { ISelectInputOptions } from "../../../app/models/common";
+import { IGoodsReceivedNote } from "../../../app/models/goodsReceivedNote";
 import GRNItemListHeader from "./GRNItemListHeader";
 import GRNItemListItem from "./GRNItemListItem";
 
-const GRNItemList = () => {
+interface IProps {
+  displayAmount: boolean;
+  displayAction: boolean;
+  grn: IGoodsReceivedNote;
+  stockTypeOptions: ISelectInputOptions[];
+}
+
+const GRNItemList: FC<IProps> = ({
+  displayAction,
+  displayAmount,
+  grn,
+  stockTypeOptions,
+}) => {
   return (
     <Fragment>
       <Table compact celled>
-        <GRNItemListHeader displayAction={false} displayAmount={true} />
-        <GRNItemListItem displayAction={false} displayAmount={true} />
+        <GRNItemListHeader
+          displayAction={displayAction}
+          displayAmount={displayAmount}
+          grn={grn}
+          stockTypeOptions={stockTypeOptions}
+        />
+        <GRNItemListItem
+          displayAction={displayAction}
+          displayAmount={displayAmount}
+          grn={grn}
+          stockTypeOptions={stockTypeOptions}
+        />
       </Table>
     </Fragment>
   );
