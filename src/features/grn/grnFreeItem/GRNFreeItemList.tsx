41752c4e03b4b3332032910eb5d92837d9ee8d1d
math2018avan <math2018avan@gmail.com>
2021-02-07 12:50:09 +0800

    Update GRNFreeItemList.tsx
    props added

--- src/features/grn/grnFreeItem/GRNFreeItemList.tsx
+++ src/features/grn/grnFreeItem/GRNFreeItemList.tsx
@@ -1,24 +1,37 @@
 import React, { Fragment, FC } from "react";
 import { Table } from "semantic-ui-react";
+import { ISelectInputOptions } from "../../../app/models/common";
+import { IGoodsReceivedNote } from "../../../app/models/goodsReceivedNote";
 import GRNFreeItemListHeader from "./GRNFreeItemListHeader";
 import GRNFreeItemListItem from "./GRNFreeItemListItem";
 
 interface IProps {
   displayAmount: boolean;
   displayAction: boolean;
+  grn: IGoodsReceivedNote;
+  stockTypeOptions: ISelectInputOptions[];
 }
 
-const GRNItemList: FC<IProps> = ({ displayAction, displayAmount }) => {
+const GRNItemList: FC<IProps> = ({
+  displayAction,
+  displayAmount,
+  grn,
+  stockTypeOptions,
+}) => {
   return (
     <Fragment>
       <Table compact celled>
         <GRNFreeItemListHeader
           displayAction={displayAction}
           displayAmount={displayAmount}
+          grn={grn}
+          stockTypeOptions={stockTypeOptions}
         />
         <GRNFreeItemListItem
           displayAction={displayAction}
           displayAmount={displayAmount}
+          grn={grn}
+          stockTypeOptions={stockTypeOptions}
         />
       </Table>
     </Fragment>
