796559c5043b1ca0eef916183085a4b32d5aac45
math2018avan <math2018avan@gmail.com>
2021-02-07 12:46:02 +0800

    Update goodsReceivedNote.ts
    grn approval model added

--- src/app/models/goodsReceivedNote.ts
+++ src/app/models/goodsReceivedNote.ts
@@ -35,3 +35,14 @@ export class CreateGoodsReceivedNote {
     }
 }
 
+
+export class ApprovalGoodsReceivedNote {
+    id: number = 0;
+    approvalStatus: string = "";
+    approvalReason: string = "";
+    constructor(id: number, status: string, reason: string) {
+        this.id = id;
+        this.approvalStatus = status;
+        this.approvalReason = reason;
+    }
+}
