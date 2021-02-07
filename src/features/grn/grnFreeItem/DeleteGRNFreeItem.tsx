fe99a0563b3fbf80f2e76f6b4a34514a5a3e002c
math2018avan <math2018avan@gmail.com>
2021-02-07 12:49:50 +0800

    Create DeleteGRNFreeItem.tsx
    delete grn free item added

---
+++ src/features/grn/grnFreeItem/DeleteGRNFreeItem.tsx
@@ -0,0 +1,49 @@
+import React, { FC, Fragment, useContext } from "react";
+import { Button, Modal, Header, Grid, Divider } from "semantic-ui-react";
+import { observer } from "mobx-react-lite";
+import { RootStoreContext } from "../../../app/stores/rootStore";
+import ErrorMessage from "../../../app/common/alert/ErrorMessage";
+import { toast } from "react-toastify";
+import { IGoodsReceivedNoteFreeItem } from "../../../app/models/goodsReceivedNoteFreeItem";
+
+interface IProps {
+  item: IGoodsReceivedNoteFreeItem;
+}
+const DeleteGRNFreeItem: FC<IProps> = ({ item }) => {
+  const rootStore = useContext(RootStoreContext);
+  const { closeModal } = rootStore.modalStore;
+  const { deleteGRNFreeItem } = rootStore.grnStore;
+  return (
+    <Fragment>
+      <Header icon="delete" content="Are you sure to delete?" color="red" />
+      <Divider horizontal></Divider>
+      <Modal.Content>
+        <p>GRN Free Item: {item.itemName}</p>
+      </Modal.Content>
+      <Divider horizontal></Divider>
+      <Modal.Actions>
+        <Grid>
+          <Grid.Column textAlign="center">
+            <Button
+              content="Yes"
+              color="red"
+              onClick={() => {
+                deleteGRNFreeItem(item.id)
+                  .then(() => {
+                    toast.success("Item deleted successfully");
+                    closeModal();
+                  })
+                  .catch((error) => {
+                    toast.error(<ErrorMessage error={error} text="Error:" />);
+                  });
+              }}
+            />
+            <Button content="No" color="green" onClick={() => closeModal()} />
+          </Grid.Column>
+        </Grid>
+      </Modal.Actions>
+    </Fragment>
+  );
+};
+
+export default observer(DeleteGRNFreeItem);
