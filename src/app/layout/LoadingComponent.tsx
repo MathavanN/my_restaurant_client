13e8463096ead62fa8bdb9b1f9af98e7533322b5
math2018avan <math2018avan@gmail.com>
2021-03-09 15:32:07 +0800

    Update LoadingComponent.tsx
    cleanup

--- src/app/layout/LoadingComponent.tsx
+++ src/app/layout/LoadingComponent.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import { FC } from "react";
 import { Dimmer, Loader } from "semantic-ui-react";
 
 export const LoadingComponent: FC<{ inverted?: boolean; content?: string }> = ({
