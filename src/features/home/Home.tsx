6b8768c958f95207683f8f77ecfca74d2f56226d
math2018avan <math2018avan@gmail.com>
2021-03-14 18:21:12 +0800

    path updated

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { PAGE_TODO_ADD } from "utils/constants";
 
 const Home = () => {
   return (
     <>
       <div>I am home This is testing.</div>
-      <Link to="/todos/newtodo">New Todo</Link>
+      <Link to={PAGE_TODO_ADD.path}>New Todo</Link>
     </>
   );
 };
