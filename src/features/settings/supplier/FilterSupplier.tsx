e53433b698402fd7f1dfd1cae52873e8215a9832
math2018avan <math2018avan@gmail.com>
2021-03-28 19:03:04 +0800

    Update FilterSupplier.tsx
    search after a filter return entire result

--- src/features/settings/supplier/FilterSupplier.tsx
+++ src/features/settings/supplier/FilterSupplier.tsx
@@ -5,15 +5,32 @@ import { Button, Form, Message } from 'semantic-ui-react';
 interface IProps {
   handleSupplierSearch: () => void;
   setPredicate: (predicate: string, value: string | number) => void;
+  removePredicate: (predicate: string) => void;
 }
 
-const FilterSupplier: FC<IProps> = ({ handleSupplierSearch, setPredicate }) => {
+const FilterSupplier: FC<IProps> = ({
+  handleSupplierSearch,
+  setPredicate,
+  removePredicate,
+}) => {
   const { handleSubmit, setValue, register, trigger } = useForm();
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const onSubmit = (data: any) => {
-    if (data.name) setPredicate('name', data.name);
-    if (data.city) setPredicate('city', data.city);
-    if (data.contactPerson) setPredicate('contactPerson', data.contactPerson);
+    if (data.name) {
+      setPredicate('name', data.name);
+    } else {
+      removePredicate('name');
+    }
+    if (data.city) {
+      setPredicate('city', data.city);
+    } else {
+      removePredicate('city');
+    }
+    if (data.contactPerson) {
+      setPredicate('contactPerson', data.contactPerson);
+    } else {
+      removePredicate('contactPerson');
+    }
 
     handleSupplierSearch();
   };
