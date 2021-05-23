938306504caecd52a6ca548865d8e8e0e1240053
math2018avan <math2018avan@gmail.com>
2021-05-23 19:37:32 +0800

    Update App.tsx
    new route added

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,26 @@ import {
   Theme,
   ThemeProvider,
 } from '@material-ui/core';
+import { useSelector } from 'react-redux';
 import Layout from './components/Layout';
 import {
   PAGE_COUNTER,
   PAGE_HOME,
   PAGE_SIGN_IN,
   PAGE_TODO,
-} from './utils/constants';
+  PAGE_UNIT_OF_MEASURE,
+} from './config/constants';
 import Home from './features/home/Home';
 import Todo from './features/todo/Todo';
 import Counter from './features/counter/Counter';
 import PrivateRoute from './components/PrivateRoute';
 import { darkTheme, lightTheme } from './theme/appTheme';
 import SignIn from './features/signIn/SignIn';
+import UnitOfMeasure from './features/unitOfMeasure/UnitOfMeasure';
+import { getIsLoggedIn } from './features/signIn/signInSlice';
 
 const App = () => {
-  const isLoggedIn = true;
+  const isLoggedIn = true; // useSelector(getIsLoggedIn);
   const [useDefaultTheme, toggle] = useReducer((theme) => !theme, true);
   let theme: Theme = createMuiTheme(useDefaultTheme ? lightTheme : darkTheme);
   theme = responsiveFontSizes(theme);
@@ -46,6 +50,12 @@ const App = () => {
                   path={PAGE_COUNTER.path}
                   exact
                   component={Counter}
+                />
+                <PrivateRoute
+                  isLoggedIn={isLoggedIn}
+                  path={PAGE_UNIT_OF_MEASURE.path}
+                  exact
+                  component={UnitOfMeasure}
                 />
               </Switch>
             )}
