dabc608c4f0b41aab17bc06c51eca727e3ec46eb
math2018avan <math2018avan@gmail.com>
2021-05-23 17:47:38 +0800

    Update constants.ts
    uom route added

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,6 +7,11 @@ export const FOOTER_HEIGHT = 30;
 export const HEADER_HEIGHT = 60;
 export const DRAWER_WIDTH = 250;
 
+// LOCAL STORAGE CONSTANTS
+export const ACCESS_TOKEN = "accessJWT";
+export const REFRESH_TOKEN = "refreshJWT";
+
+// ROUTE CONSTANTS
 export const PAGE_HOME = { path: '/', title: 'Home', tooltip: 'Home' };
 export const PAGE_SIGN_IN = { path: '/sign-in' };
 export const PAGE_COUNTER = {
@@ -15,3 +20,4 @@ export const PAGE_COUNTER = {
   tooltip: 'Counter',
 };
 export const PAGE_TODO = { path: '/todo', title: 'Todo', tooltip: 'Todo' };
+export const PAGE_UNIT_OF_MEASURE = { path: '/unit-of-measure', title: 'Unit Of Measure', tooltip: 'Unit Of Measure' }
